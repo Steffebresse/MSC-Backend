1676bc1a001020def456c26b0c8dd8d2dc09c3b2
Steffebresse <stefan.kureljusic@outlook.com>
2025-08-18 20:03:35 +0200

    IMplemented Posts class

--- models/DatabaseModels/Discussion.cs
+++ models/DatabaseModels/Discussion.cs
@@ -1,22 +1,35 @@
 
 
+using System.ComponentModel.DataAnnotations;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
-public class Discussion
+public class Discussion // Dependent since, this need to exist for a post to exist
 {
     public Guid Id { get; set; }
     public string Title { get; set; }
     public Guid MovieId { get; set; }
-    public Movie? Movie { get; set; } = null;
+    [Required]
+    public Movie Movie { get; set; } 
     public Guid UserId { get; set; }
     public ApplicationUser? User { get; set; }
-    public List<Posts>? Posts { get; set; }
+    public List<Post>? Posts { get; set; }
+    public DateTime PostedAt { get; set; } = DateTime.Now;
 
 
 
 }
 
-public class Posts 
+public class Post // Principal since, this existing is based on the Dependent entity
 {
-    
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime PostedAt { get; set; } = DateTime.Now;
+    [Required]
+    public Guid DisucssionId { get; set; }
+    [Required]
+    public Discussion Discussion { get; set; }
+    [Required]
+    public ApplicationUser User { get; set; }
+    [Required]
+    public Guid UserId { get; set; }
+
 }
