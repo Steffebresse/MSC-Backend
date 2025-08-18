6cf98b5a43af8e00902a2c16fc36501a274cabcf
Steffebresse <stefan.kureljusic@outlook.com>
2025-08-18 21:02:09 +0200

    Made an todo

--- obj/Debug/net9.0/MSC Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/MSC Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d65af343cc33fb12addb8d85b4f63d9435448ae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7341742fae359ca7522295c211bbc05c5af9ca3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
