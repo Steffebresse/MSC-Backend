8dd8d9fe509eec489cb91619cb568c617a9e24df
Steffebresse <stefan.kureljusic@outlook.com>
2025-09-03 19:39:45 +0200

    Failed attempt to force identity tokens to authenticate

--- obj/Debug/net9.0/MSC Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/MSC Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beec75529a242477f3a981b24362b2cb78e20758")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ceb05b994561610ee1fbac67fb7e24c3cf11ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
