90cc104d8abe856fa289f8a0a12a2ca71363a131
Steffebresse <stefan.kureljusic@outlook.com>
2025-09-21 17:17:06 +0200

    Push

--- obj/Debug/net9.0/MSC Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/MSC Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+451e99d1887bc8daeecdc71fa61648cfa992bf0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94977bb4a56733ac20402a3c11226e8fc828b171")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
