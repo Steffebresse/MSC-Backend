451e99d1887bc8daeecdc71fa61648cfa992bf0e
Steffebresse <stefan.kureljusic@outlook.com>
2025-09-21 16:45:50 +0200

    Started front end project

--- obj/Debug/net9.0/MSC Backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/MSC Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11cc1dd72d7ac056bbde4c6482e6d586f43e652e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cdd59db804e8f51693a8012d73de0fd1145c64f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MSC Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
